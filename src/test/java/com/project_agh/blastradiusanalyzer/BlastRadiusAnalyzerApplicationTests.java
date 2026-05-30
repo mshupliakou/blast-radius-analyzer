8d34b254a3c222aa5c1ac6c8918bf74c117c6daa
mshupliakou <mikhail.shupliakou@gmail.com>
2026-05-30 14:16:25 +0200

    initial commit

--- src/test/java/com/project_agh/blastradiusanalyzer/BlastRadiusAnalyzerApplicationTests.java
+++ src/test/java/com/project_agh/blastradiusanalyzer/BlastRadiusAnalyzerApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class BlastRadiusAnalyzerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
