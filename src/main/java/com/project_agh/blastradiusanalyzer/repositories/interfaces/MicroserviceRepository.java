238003daf1cbfe3b5834e384e7e50943c833595b
mshupliakou <mikhail.shupliakou@gmail.com>
2026-06-01 14:18:27 +0200

    new methods were added

--- src/main/java/com/project_agh/blastradiusanalyzer/repositories/interfaces/MicroserviceRepository.java
+++ src/main/java/com/project_agh/blastradiusanalyzer/repositories/interfaces/MicroserviceRepository.java
@@ -10,4 +10,9 @@ public interface MicroserviceRepository {
     void createDependency(String sourceId, String targetId);
     List<Microservice> getBlastRadius(String targetId);
     Map<String, Object> getTopology();
+    void deleteMicroservice(String id);
+    void deleteDependency(String sourceId, String targetId);
+    void createCluster(String name, String color, List<String> nodeIds);
+    void deleteNode(String id);
+    void addNote(String title, String text, String color, String targetType, String targetId);
 }
